cf9d89e3d32cd0c31edb2f2f2562b440e36c112c
Rahul <rmunakala@evoketechnologies.com>
2018-04-20 18:17:25 +0530

    [Base setup] - First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1> Hi, I am component-1 </h1>
+        <p> This is really working! </p>
+      </div>
+    );
+  }
+}
+
+export default App;
